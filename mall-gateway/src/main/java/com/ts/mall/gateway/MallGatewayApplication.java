be719c91473606f2401a3e4f9602e9bc4e810340
pxj <271501722@qq.com>
2020-04-23 22:10:21 +0800

    gateway 配置

---
+++ mall-gateway/src/main/java/com/ts/mall/gateway/MallGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.ts.mall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ *  开启注册发现
+ */
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class MallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallGatewayApplication.class, args);
+    }
+
+}
