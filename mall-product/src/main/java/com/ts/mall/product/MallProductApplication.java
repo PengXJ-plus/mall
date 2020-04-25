f205757145054a1cfc27dc1c22a888b0d170c996
pxj <271501722@qq.com>
2020-04-25 16:14:24 +0800

    配置mapper扫描基类

--- mall-product/src/main/java/com/ts/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/ts/mall/product/MallProductApplication.java
@@ -1,10 +1,13 @@
 package com.ts.mall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
 @EnableDiscoveryClient
+@MapperScan("com.ts.mall.product.dao")
 @SpringBootApplication
 public class MallProductApplication {
 
