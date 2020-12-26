e5bd89cdd33c2d3cba186ac153871892b726ffa5
pxj <271501722@qq.com>
2020-12-26 19:15:06 +0800

    权限验证

---
+++ mall-fast/src/main/java/io/renren/modules/sys/service/ShiroService.java
@@ -0,0 +1,26 @@
+package io.renren.modules.sys.service;
+
+import io.renren.modules.sys.entity.SysUserEntity;
+import io.renren.modules.sys.entity.SysUserTokenEntity;
+
+import java.util.Set;
+
+/**
+ * shiro相关接口
+ *
+ * @author Mark PENGXJ
+ */
+public interface ShiroService {
+    /**
+     * 获取用户权限列表
+     */
+    Set<String> getUserPermissions(long userId);
+
+    SysUserTokenEntity queryByToken(String token);
+
+    /**
+     * 根据用户ID，查询用户
+     * @param userId
+     */
+    SysUserEntity queryUser(Long userId);
+}
